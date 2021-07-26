8d826fc5b9ac36d9c8cccf33594bec07df999345
Sreyeesh Garimella <sgarime1@gmail.com>
2021-07-25 23:20:29 -0700

    started milestone project

--- The_Complete_Python_Course/movie_collection_app/movie_collection.py
+++ The_Complete_Python_Course/movie_collection_app/movie_collection.py
@@ -5,16 +5,18 @@ movies = []
 
 
 # You may want to create a function for this code
-title = input("Enter the movie title: ")
-director = input("Enter the movie director: ")
-year = input("Enter the movie release year: ")
+def add_to_movie_database():
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movie release year: ")
 
-movies.append({
+    movies.append({
     'title': title,
     'director': director,
     'year': year
 })
 
+add_to_movie_database()
 
 # Create other functions for:
 #   - listing movies
@@ -22,15 +24,16 @@ movies.append({
 
 
 # And another function here for the user menu
-selection = input(MENU_PROMPT)
-while selection != 'q':
-    if selection == "a":
-        pass
-    elif selection == "l":
-        pass
-    elif selection == "f":
-        pass
-    else:
-        print('Unknown command. Please try again.')
+def menu_selection():
+    selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == "a":
+            pass
+        elif selection == "l":
+            pass
+        elif selection == "f":
+            pass
+        else:
+            print('Unknown command. Please try again.')
 
     selection = input(MENU_PROMPT)

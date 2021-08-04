447f1a6ec3824f72363667ecde51ddade4585a54
Sreyeesh <Sgarime1@gmail.com>
2021-08-04 01:06:07 -0700

    updates

--- The_Complete_Python_Course/Opposites_Attract.py
+++ The_Complete_Python_Course/Opposites_Attract.py
@@ -7,15 +7,13 @@ Write a function that will take the number of petals of each flower and return t
 """
 
 #picked a flower each
-flower1 =  range(1,101)
-flower2 =  range(34,580)
+flower1 =  range(1, 101)
+flower2 =  range(34, 580)
 
 def lovefunc(flower1,flower2):
-    
- 
     for petal1 in flower1:
         for petal2 in flower2:
-            if petal1 % 2 == 0 and petal2 % 2 == 1  or petal2 % 2 == 0 and petal1 % 2 ==1: 
+            if (petal1 % 2 == 0 and petal2 % 2 == 1)  or (petal2 % 2 == 0 and petal1 % 2 == 1): 
                 return True
                 print('flower one petals picked ',petal1,' petals pick  ', petal2,'Timmy and Sarah are in love')
                 
@@ -25,3 +23,8 @@ def lovefunc(flower1,flower2):
                 
     
 print(lovefunc(flower1,flower2))     
+
+
+
+# Operators precedence
+

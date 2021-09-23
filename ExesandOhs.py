1af9e0728501b1403654ce3f13e96101a0e21204
Sreyeesh Garimella <sgarime1@gmail.com>
2021-09-22 23:25:26 -0700

    udpates

--- ExesandOhs.py
+++ ExesandOhs.py
@@ -11,6 +11,25 @@ XO("zzoo") => false
 
 """
 
+
+# Check to  see if a string has same amount 'x' and 'o'
+# you can use count to count strings
+
+s ='xo'
+
 def xo(s):
-    
-    return True 
+    letters = s
+    x_count = 0 
+    o_count = 0 
+
+    for i in letters:
+        if i.lower() == 'x':
+             x_count += 1
+        elif i.lower() == 'o':
+            o_count += 1
+
+    return x_count == o_count 
+
+
+print(xo(s))
+  

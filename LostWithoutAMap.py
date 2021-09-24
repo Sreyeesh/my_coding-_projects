4129408b8e45a71bf32125aa7cf10d6c31129d26
Sreyeesh Garimella <sgarime1@gmail.com>
2021-09-23 23:34:41 -0700

    updates

--- LostWithoutAMap.py
+++ LostWithoutAMap.py
@@ -19,14 +19,15 @@ For example:
 list_of_numbers = [1,2,3]
 
 def maps(a):
-    
-# def maps(a):
-#     new_list_of_numbers =  []
+    new_list_of_numbers =  []
 
-#     for i in range(len(a)):
-#         a[i] *= 2
-#         new_list_of_numbers += [a[i]]
+    for i in range(len(a)):
+        # print('this is a number ',i)
+        a[i] *= 2
+        # print('numbers',a[i])
+        new_list_of_numbers += [a[i]]
+        # print('more numbers',new_list_of_numbers)
 
-#     return new_list_of_numbers
+    return new_list_of_numbers
     
-# print(maps(list_of_numbers))  
+print(maps(list_of_numbers))  
